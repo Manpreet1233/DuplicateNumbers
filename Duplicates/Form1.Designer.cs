0938a14924416668c43d54a969a78bfc1055ef99
Manpreet Rajpal <manpreetrajpal7@gmail.com>
2017-10-05 20:49:39 -0400

    first commit

---
+++ Duplicates/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Duplicates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileDisplay = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnDuplicate = new System.Windows.Forms.Button();
+            this.lblDuplicate = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(543, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileOpen,
+            this.toolStripMenuItem1,
+            this.mnuFileDisplay,
+            this.toolStripMenuItem2,
+            this.mnuFileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.Size = new System.Drawing.Size(112, 22);
+            this.mnuFileOpen.Text = "Open";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // mnuFileDisplay
+            // 
+            this.mnuFileDisplay.Name = "mnuFileDisplay";
+            this.mnuFileDisplay.Size = new System.Drawing.Size(112, 22);
+            this.mnuFileDisplay.Text = "Display";
+            this.mnuFileDisplay.Click += new System.EventHandler(this.mnuFileDisplay_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(109, 6);
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(112, 22);
+            this.mnuFileExit.Text = "Exit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(13, 39);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(41, 13);
+            this.lblDisplay.TabIndex = 1;
+            this.lblDisplay.Text = "Display";
+            this.lblDisplay.Visible = false;
+            // 
+            // btnDuplicate
+            // 
+            this.btnDuplicate.Location = new System.Drawing.Point(82, 34);
+            this.btnDuplicate.Name = "btnDuplicate";
+            this.btnDuplicate.Size = new System.Drawing.Size(75, 23);
+            this.btnDuplicate.TabIndex = 2;
+            this.btnDuplicate.Text = "Duplicate";
+            this.btnDuplicate.UseVisualStyleBackColor = true;
+            this.btnDuplicate.Visible = false;
+            this.btnDuplicate.Click += new System.EventHandler(this.btnDuplicate_Click);
+            // 
+            // lblDuplicate
+            // 
+            this.lblDuplicate.AutoSize = true;
+            this.lblDuplicate.Location = new System.Drawing.Point(104, 60);
+            this.lblDuplicate.Name = "lblDuplicate";
+            this.lblDuplicate.Size = new System.Drawing.Size(97, 13);
+            this.lblDuplicate.TabIndex = 3;
+            this.lblDuplicate.Text = "Duplicate Numbers";
+            this.lblDuplicate.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(543, 433);
+            this.Controls.Add(this.lblDuplicate);
+            this.Controls.Add(this.btnDuplicate);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileDisplay;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnDuplicate;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label lblDuplicate;
+    }
+}
+
---
+++ Duplicates/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+//Manpreet Rajpal 
+//September 29, 2017
+//This program displays the array of numbers and than shows the duplicate of the array without the repeated numbers
+namespace Duplicates
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        int[] Numbers;
+        //int Counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+
+            //create fileDialog in memory and than show it
+            //checks to see if any file is seleted "DialogResult.Ok"
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                //open the file to read the numbers (data) 
+                //allows us to read the content of a txt file 
+                StreamReader sr = new StreamReader(fd.OpenFile());
+
+                int OpenedFile = int.Parse(sr.ReadLine());
+                Numbers = new int[OpenedFile];
+
+                //insert marks into array
+                //records stores how many times you have to read in 
+                for (int i = 0; i < OpenedFile; i++)
+                {
+                    Numbers[i] = int.Parse(sr.ReadLine());
+                }
+
+                MessageBox.Show("File loaded Sucessfully!");
+            }
+        }
+
+        private void mnuFileDisplay_Click(object sender, EventArgs e)
+        {
+            //display each mark stored in array
+            string Output = String.Empty; //same as ""
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Output = Output + Numbers[i] + "\n";
+            }
+
+            lblDisplay.Text = Output;
+            lblDisplay.Visible = true;
+            btnDuplicate.Visible = true;
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            //closes the program when pressing exit
+            //Application.Exit();
+
+            if (MessageBox.Show("Do you really want to Exit?", "WinMarks", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                //closes the program when pressing exit
+                Application.Exit();
+            }
+        }
+
+        private void btnDuplicate_Click(object sender, EventArgs e)
+        {
+            //create Duplicate array to copy the original numbers into without repeated numbers
+           
+            //inserts a negative to the numbers that are getting repeated 
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                for (int j = i + 1; j < Numbers.Length; j++)
+                {
+                    if (Numbers[i] == Numbers[j])
+                    {
+                        Numbers[j] = -1;
+                    }
+                }
+            }
+            //counts the number of -1's and subtracts it from array length
+            int Counter = 0;
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] == -1) 
+                {
+                    Counter++;
+                }
+            }
+            int[] Duplicate = new int[Numbers.Length - Counter];
+
+            int InsertIndex = 0;
+
+            //copying the values over to the Duplicate array
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] != -1)
+                {
+                    Duplicate[InsertIndex] = Numbers[i];
+                    InsertIndex++;
+                }
+            }
+            //displays duplicate array
+            string Output = String.Empty; //same as ""
+
+            for (int i = 0; i < Duplicate.Length; i++)
+            {
+                Output = Output + Duplicate[i] + "\n";
+            }
+
+            lblDuplicate.Text = Output;
+            lblDuplicate.Visible = true; 
+        }
+    }
+}
